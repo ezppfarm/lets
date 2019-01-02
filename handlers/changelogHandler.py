db51f27f1f42956868a93e5abe882624e15390e0
solis <thecriti@gmail.com>
2019-01-02 23:51:28 +1030

    Update changelogHandler.py

--- handlers/changelogHandler.py
+++ handlers/changelogHandler.py
@@ -46,7 +46,7 @@ class handler(requestsManager.asyncRequestHandler):
 
         try:
             lines = []
-            with open("/home/ripple/other/changelog.txt", "r") as f:
+            with open("../ci_meme/changelog.txt", "r") as f:
                 for i, l in enumerate(f):
                     if i >= 100:
                         break
