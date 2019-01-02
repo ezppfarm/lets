23fd749e369867cab6623fb37a5ef0e293625133
depreciate <thecriti@gmail.com>
2019-01-03 02:53:35 +1030

    pp changes

--- objects/scoreAuto.pyx
+++ objects/scoreAuto.pyx
@@ -7,15 +7,15 @@ from common.ripple import userUtils
 from constants import rankedStatuses
 from common.ripple import scoreUtils
 from objects import glob
-from pp import rippoppai
+from pp import autooppai
 from pp import wifipiano2
 from pp import cicciobello
 
 
 class score:
 	PP_CALCULATORS = {
-		gameModes.STD: rippoppai.oppai,
-		gameModes.TAIKO: rippoppai.oppai,
+		gameModes.STD: autooppai.oppai,
+		gameModes.TAIKO: autooppai.oppai,
 		gameModes.CTB: cicciobello.Cicciobello,
 		gameModes.MANIA: wifipiano2.piano
 	}
--- objects/scoreRelax.pyx
+++ objects/scoreRelax.pyx
@@ -7,15 +7,15 @@ from common.ripple import userUtils
 from constants import rankedStatuses
 from common.ripple import scoreUtils
 from objects import glob
-from pp import rippoppai
+from pp import relaxoppai
 from pp import wifipiano2
 from pp import cicciobello
 
 
 class score:
 	PP_CALCULATORS = {
-		gameModes.STD: rippoppai.oppai,
-		gameModes.TAIKO: rippoppai.oppai,
+		gameModes.STD: relaxoppai.oppai,
+		gameModes.TAIKO: relaxoppai.oppai,
 		gameModes.CTB: cicciobello.Cicciobello,
 		gameModes.MANIA: wifipiano2.piano
 	}
