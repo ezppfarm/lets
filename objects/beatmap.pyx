07535ed19a41a6f5b36ebe65f5d7db2ca473b9dd
depreciate <thecriti@gmail.com>
2019-01-05 04:50:37 +1030

    lets try this,.

--- objects/beatmap.pyx
+++ objects/beatmap.pyx
@@ -65,7 +65,9 @@ class beatmap:
 			frozen = 0
 
 		# Add new beatmap data
+		
 		log.debug("Saving beatmap data in db...")
+		glob.db.execute("DELETE FROM beatmaps WHERE id = %s LIMIT 1", [bdata["id"]])
 		glob.db.execute("INSERT INTO `beatmaps` (`id`, `beatmap_id`, `beatmapset_id`, `beatmap_md5`, `song_name`, `ar`, `od`, `difficulty_std`, `difficulty_taiko`, `difficulty_ctb`, `difficulty_mania`, `max_combo`, `hit_length`, `bpm`, `ranked`, `latest_update`, `ranked_status_freezed`) VALUES (NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", [
 			self.beatmapID,
 			self.beatmapSetID,
