5c18766d02beb4690772cdbe4c9b8087004c0642
ilyt <contact@ilyt.cc>
2019-01-05 12:46:19 +0000

    fuck

--- handlers/submitModularHandler.pyx
+++ handlers/submitModularHandler.pyx
@@ -482,7 +482,7 @@ class handler(requestsManager.asyncRequestHandler):
 									beatmapInfo.songName.encode().decode("ASCII", "ignore"),
 									gameModes.getGamemodeFull(s.gameMode)
 								)
-								log.info(newScoreboard.scores)
+								log.info(newScoreboard.scores) #
 								userUtils.logUserLogX("has lost first place Vanilla on ",s.fileMd5, newScoreboard.scores[1].playerUserID, s.gameMode, s.rank)	
 								params = urlencode({"k": glob.conf.config["server"]["apikey"], "to": "#announce", "msg": annmsg})
 								requests.get("{}/api/v1/fokabotMessage?{}".format(glob.conf.config["server"]["banchourl"], params))
