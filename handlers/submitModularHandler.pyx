36d13d209f4e881c06ef635592474c7b1d4c03a2
ilyt <contact@ilyt.cc>
2019-01-05 12:57:42 +0000

    comment it out

--- handlers/submitModularHandler.pyx
+++ handlers/submitModularHandler.pyx
@@ -482,8 +482,8 @@ class handler(requestsManager.asyncRequestHandler):
 									beatmapInfo.songName.encode().decode("ASCII", "ignore"),
 									gameModes.getGamemodeFull(s.gameMode)
 								)
-								log.info(newScoreboard.scores) #
-								userUtils.logUserLogX("has lost first place Vanilla on ",s.fileMd5, newScoreboard.scores[1].playerUserID, s.gameMode, s.rank)	
+								#log.info(newScoreboard.scores) #
+								#userUtils.logUserLogX("has lost first place Vanilla on ",s.fileMd5, newScoreboard.scores[1].playerUserID, s.gameMode, s.rank)	
 								params = urlencode({"k": glob.conf.config["server"]["apikey"], "to": "#announce", "msg": annmsg})
 								requests.get("{}/api/v1/fokabotMessage?{}".format(glob.conf.config["server"]["banchourl"], params))
 				if isRelaxing:
