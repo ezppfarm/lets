227b69e045948ef6697f7dee13864f1458a02803
depreciate <thecriti@gmail.com>
2019-01-15 07:57:45 +1030

    d

--- objects/beatmap.pyx
+++ objects/beatmap.pyx
@@ -37,7 +37,7 @@ class beatmap:
 		self.hitLength = 0
 		self.bpm = 0
 		
-		self.rankingDate = int(time.mktime(datetime.datetime.strptime(mainData["last_update"], "%Y-%m-%d %H:%M:%S").timetuple()))
+		self.rankingDate = 0
 
 		# Statistics for ranking panel
 		self.playcount = 0
@@ -168,6 +168,7 @@ class beatmap:
 		self.maxCombo = int(data["max_combo"])
 		self.hitLength = int(data["hit_length"])
 		self.bpm = int(data["bpm"])
+		self.rankingDate = int(time.mktime(datetime.datetime.strptime(mainData["last_update"], "%Y-%m-%d %H:%M:%S").timetuple()))
 		# Ranking panel statistics
 		self.playcount = int(data["playcount"]) if "playcount" in data else 0
 		self.passcount = int(data["passcount"]) if "passcount" in data else 0
