81ea396792de7d5e7c0cafc0a8a195fa866e4420
depreciate <thecriti@gmail.com>
2019-02-27 21:07:11 +1030

    remove logging from these

--- handlers/submitModularHandler.pyx
+++ handlers/submitModularHandler.pyx
@@ -338,8 +338,6 @@ class handler(requestsManager.asyncRequestHandler):
 				oldRank = userUtils.getGameRank(userID, s.gameMode)
 
 			log.debug("Updating {}'s stats...".format(username))
-			log.warning("logging s.gamemode {}".format(s.gameMode))
-			log.warning("logging user's max combo {}".format(maxCombo))
 			if UsingRelax:	
 				userUtils.updateStatsRx(userID, s)
 			if UsingAuto:	
